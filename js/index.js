afabf4a96596d40b543fd047e42edfd3ace0f035
Cauê Grande Yanagihara <134626371+CaueYanagihara@users.noreply.github.com>
2024-07-30 20:46:43 -0300

    refactor(Home Page): Ajuste dos produtos na NavBar
    refactor(Home Page): Ajuste dos produtos na NavBar
    
    Ajustado o Wallet e o Notify no NavBar.
    
    PL-#15

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 // MENUS DROP DOWN DA NAV
-document.querySelector('.nav-item-toggle').addEventListener('click', function() {
+document.querySelector('.nav-item-toggle').addEventListener('click', function () {
     classToggle('.nav-list', 'nav-toggle-show');
-});
-
-document.querySelector('.nav-sub-item-toggle').addEventListener('click', function() {
-    classToggle('.nav-sub-list', 'nav-toggle-show');
 });
 
 function classToggle(targetSelector, toggleClass) {
@@ -13,10 +9,9 @@ function classToggle(targetSelector, toggleClass) {
     elements.forEach(element => element.classList.toggle(toggleClass));
 }
 
-// OPCAO QUE FOI CLICADA NA NAV
 document.querySelectorAll('.nav-item').forEach(item => {
-    item.addEventListener('click', function() {
+    item.addEventListener('click', function () {
         document.querySelectorAll('.nav-item').forEach(i => i.classList.remove('active'));
-        this.classList.add('active'); 
+        this.classList.add('active');
     });
 });
